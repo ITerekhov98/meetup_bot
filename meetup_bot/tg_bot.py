3c0bab2a37661d8b676ca950b5e6caec355ee479
hyggebox <asya_apto@mail.ru>
2022-07-29 17:01:38 +0300

    Fix msg text in ask_donation_sum()

--- meetup_bot/tg_bot.py
+++ meetup_bot/tg_bot.py
@@ -218,7 +218,7 @@ def ask_donation_sum(update: Update, context: CallbackContext):
     )
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Введите сумму доната в рублях числом (не менее 60)',
+        text='Введите сумму доната в рублях числом не менее 60 :)',
         reply_markup=reply_markup
     )
     return 'HANDLE_DONATION'
